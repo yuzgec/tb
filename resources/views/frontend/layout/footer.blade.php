326f9bf5b0e5f116baadddf946da6c7d80848595
yuzgec <olcayy@gmail.com>
2022-08-19 15:33:42 +0300

    Fix

--- resources/views/frontend/layout/footer.blade.php
+++ resources/views/frontend/layout/footer.blade.php
@@ -59,24 +59,24 @@
     <div class="footer-newsletter bg-image" style="background-image: url(/frontend/assets/images/backgrounds/bg-2.jpg)">
         <div class="container">
             <div class="heading text-center">
-                <h3 class="title">Get The Latest Deals</h3><!-- End .title -->
-                <p class="title-desc">and receive <span>$20 coupon</span> for first shopping</p><!-- End .title-desc -->
-            </div><!-- End .heading -->
+                <h3 class="title">GHaber Bülteminize Katılın</h3><!-- End .title -->
+                <p class="title-desc">Sitemize yüklenen kitap ve ürünlerden ilk siz haberdar olun</p><!-- End .title-desc -->
+            </div>
 
             <div class="row">
                 <div class="col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-6 offset-lg-3">
                     <form action="#">
                         <div class="input-group">
-                            <input type="email" class="form-control" placeholder="Enter your Email Address" aria-label="Email Adress" aria-describedby="newsletter-btn" required>
+                            <input type="email" class="form-control" placeholder="Email Adresinizi Giriniz" aria-label="Email Adress" aria-describedby="newsletter-btn" required>
                             <div class="input-group-append">
-                                <button class="btn btn-primary" type="submit" id="newsletter-btn"><span>Subscribe</span><i class="icon-long-arrow-right"></i></button>
-                            </div><!-- .End .input-group-append -->
-                        </div><!-- .End .input-group -->
+                                <button class="btn btn-primary" type="submit" id="newsletter-btn"><span>Abone Ol</span><i class="icon-long-arrow-right"></i></button>
+                            </div>
+                        </div>
                     </form>
-                </div><!-- End .col-sm-10 offset-sm-1 col-lg-6 offset-lg-3 -->
-            </div><!-- End .row -->
-        </div><!-- End .container -->
-    </div><!-- End .footer-newsletter bg-image -->
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="footer-middle">
         <div class="container">
             <div class="row">
