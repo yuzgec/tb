e548388ea06b7c57e033226d5c7aa3f13ce0eed7
yuzgec <olcayy@gmail.com>
2022-08-22 12:56:51 +0300

    Fix

--- app/Http/Controllers/PublisherController.php
+++ app/Http/Controllers/PublisherController.php
@@ -16,12 +16,19 @@ class PublisherController extends Controller
 
     public function create()
     {
-        //
+        return view('backend.publisher.create');
     }
 
     public function store(Request $request)
     {
-        //
+        $New = new Publisher;
+        $New->title = $request->title;
+
+        $New->save();
+
+        toast(SWEETALERT_MESSAGE_CREATE,'success');
+        return view('backend.publisher.index');
+
     }
 
     public function show($id)
@@ -31,17 +38,28 @@ class PublisherController extends Controller
 
     public function edit($id)
     {
-        //
+        $Edit = Publisher::findOrFail($id);
+        return view('backend.publisher.edit', compact('Edit'));
     }
 
     public function update(Request $request, $id)
     {
-        //
+        $Update = new Publisher;
+        $Update->title = $request->title;
+
+        $Update->save();
+
+        toast(SWEETALERT_MESSAGE_CREATE,'success');
+        return view('backend.publisher.index');
     }
 
     public function destroy($id)
     {
-        //
+        $Delete = Publisher::findOrFail($id);
+        $Delete->delete();
+
+        toast(SWEETALERT_MESSAGE_DELETE,'success');
+        return redirect()->route('publisher.index');
     }
 
     public function getOrder(Request $request){
