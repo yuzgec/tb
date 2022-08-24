94c34db4decc7823cc22f87b3839defb8abefb3a
yuzgec <olcayy@gmail.com>
2022-08-24 03:41:49 +0300

    Fix

--- resources/views/backend/translator/index.blade.php
+++ resources/views/backend/translator/index.blade.php
@@ -58,7 +58,7 @@
                                         Eylemler
                                     </button>
                                     <div class="dropdown-menu dropdown-menu-end">
-                                        <a class="dropdown-item justify-content-between" href="{{ route('author.edit', $item->id) }}" title="Düzenle">
+                                        <a class="dropdown-item justify-content-between" href="{{ route('translator.edit', $item->id) }}" title="Düzenle">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 15l8.385 -8.415a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3z" /><path d="M16 5l3 3" /><path d="M9 7.07a7.002 7.002 0 0 0 1 13.93a7.002 7.002 0 0 0 6.929 -5.999" /></svg>
                                             Düzenle
                                         </a>
@@ -87,7 +87,7 @@
                                         <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 13l-4 -4l4 -4m-4 4h11a4 4 0 0 1 0 8h-1" /></svg>
                                         İptal Et
                                     </a>
-                                    <form action="{{ route('author.destroy', $item->id) }}" method="POST">
+                                    <form action="{{ route('translator.destroy', $item->id) }}" method="POST">
                                         @csrf
                                         @method('DELETE')
                                         <button type="submit" class="btn btn-danger btn-sm ms-auto">
