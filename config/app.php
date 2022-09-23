68646916c453bfb34342d61ab52c6eaa0d3ce89e
yuzgec <olcayy@gmail.com>
2022-09-24 02:27:03 +0300

    Fix

--- config/app.php
+++ config/app.php
@@ -189,7 +189,6 @@ return [
          * Package Service Providers...
          */
         Intervention\Image\ImageServiceProvider::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
 
         /*
          * Application Service Providers...
