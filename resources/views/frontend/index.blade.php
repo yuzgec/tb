8078db860c7b21e67e3867b0206d5714974af30d
yuzgec <olcayy@gmail.com>
2022-10-17 15:39:07 +0300

    Fix

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -143,7 +143,7 @@
 
     </div>
 
-    <div class="blog-posts">
+    <div class="blog-posts  mt-3">
         <div class="container">
             <div class="owl-carousel owl-simple carousel-with-shadow" data-toggle="owl"
                  data-owl-options='{
@@ -190,281 +190,6 @@
 
         </div>
     </div>
-{{--
-
-    <div class="block mt-5">
-        <div class="block-wrapper ">
-            <div class="container">
-                <div class="heading heading-flex">
-                    <div class="heading-left">
-                        <h2 class="title">Türk Edebiyatı</h2>
-                    </div>
-
-                    <div class="heading-right">
-                        <a href="category.html" class="title-link">Hepsini Görüntüle <i class="icon-long-arrow-right"></i></a>
-                    </div>
-                </div>
-
-                <div class="owl-carousel carousel-equal-height owl-simple" data-toggle="owl"
-                     data-owl-options='{
-                                "nav": false,
-                                "dots": true,
-                                "margin": 20,
-                                "loop": false,
-                                "responsive": {
-                                    "0": {
-                                        "items":2
-                                    },
-                                    "480": {
-                                        "items":2
-                                    },
-                                    "768": {
-                                        "items":3
-                                    },
-                                    "992": {
-                                        "items":4
-                                    },
-                                    "1200": {
-                                        "items":5
-                                    },
-                                    "1440": {
-                                        "items":6
-                                    }
-                                }
-                            }'>
-                    @foreach($Product->take(8) as $item)
-                        <div class="product product-2 text-center">
-                            <span class="product-label label-circle label-new">Yeni</span>
-                            <figure class="product-media">
-                                <a href="{{ route('urun' , $item->slug)}}" title="{{ $item->title }}">
-                                    <img class="img-fluid" src="{{ (!$item->getFirstMediaUrl('page')) ? '/resimyok.jpg' : $item->getFirstMediaUrl('page', 'thumb')}}" alt="{{ $item->title }}">
-                                </a>
-
-                                <div class="product-action-vertical">
-                                    <a href="#" class="btn-product-icon btn-wishlist"><span>Favorilere Ekle</span></a>
-                                </div>
-                            </figure>
-
-                            <div class="product-body">
-                                <h3 class="product-title">
-                                    <a href="{{ route('urun' , $item->slug)}}" title="{{ $item->title }}">
-                                    {{ $item->title }}
-                                </h3>
-                                <div class="product-price">
-                                    {{ $item->price }}₺
-                                </div>
-                            </div>
-                            <div class="product-action">
-                                <a href="{{ route('urun' , $item->slug)}}"
-                                   title="{{ $item->title }}"
-                                   class="btn-product btn-cart">
-                                    <span>Sepete Ekle</span>
-                                </a>
-                            </div>
-                        </div>
-                    @endforeach
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="mb-5"></div>
-
-
-
-
-    <div class="bestseller-products bg-light pt-5 pb-5 mb-5">
-        <div class="block">
-            <div class="block-wrapper ">
-                <div class="container">
-                    <div class="heading heading-flex">
-                        <div class="heading-left">
-                            <h2 class="title">Çok Bakılanlar</h2>
-                        </div>
-
-                        <div class="heading-right">
-                            <a href="category.html" class="title-link">Hepsini Görüntüle <i class="icon-long-arrow-right"></i></a>
-                        </div>
-                    </div>
-
-
-                    <div class="owl-carousel carousel-equal-height owl-simple" data-toggle="owl"
-                         data-owl-options='{
-                                "nav": false,
-                                "dots": true,
-                                "margin": 20,
-                                "loop": false,
-                                "responsive": {
-                                    "0": {
-                                        "items":2
-                                    },
-                                    "480": {
-                                        "items":2
-                                    },
-                                    "768": {
-                                        "items":3
-                                    },
-                                    "992": {
-                                        "items":4
-                                    },
-                                    "1200": {
-                                        "items":5
-                                    },
-                                    "1440": {
-                                        "items":6
-                                    }
-                                }
-                            }'>
-                        @foreach($Product->take(8) as $item)
-
-                            <div class="product">
-                                <figure class="product-media">
-                                    <span class="product-label label-sale">Sale</span>
-                                    <a href="product.html">
-                                        <img src="/frontend/assets/images/demos/demo-19/products/product-1.jpg" alt="Product image" class="product-image">
-                                    </a>
-
-                                    <div class="product-action-vertical">
-                                        <a href="#" class="btn-product-icon btn-wishlist" title="Add to Wishlist"><span>add to wishlist</span></a>
-                                    </div><!-- End .product-action-vertical -->
-
-                                    <div class="product-action">
-                                        <a href="#" class="btn-product btn-cart"><span>add to cart</span></a>
-                                    </div><!-- End .product-action -->
-                                </figure><!-- End .product-media -->
-
-                                <div class="product-body">
-                                    <div class="product-cat">
-                                        <a href="#">Shooter</a>
-                                    </div><!-- End .product-cat -->
-                                    <h3 class="product-title"><a href="product.html">Call of Duty <br>WWII - Gold Edition</a></h3><!-- End .product-title -->
-                                    <div class="product-price">
-                                        <span class="new-price">$24.00</span>
-                                        <span class="old-price">Was $59.99</span>
-                                    </div><!-- End .product-price -->
-                                </div><!-- End .product-body -->
-                            </div>
-                        @endforeach
-                    </div>
-                </div><!-- End .container -->
-            </div><!-- End .block-wrapper -->
-        </div><!-- End .block -->
-    </div><!-- End .bg-light pt-4 pb-4 -->
-
-
-    <div class="display-row bg-light">
-        <div class="container-fluid">
-            <div class="row align-items-center">
-                <div class="col-lg-6 order-lg-last">
-                    <div class="banner banner-overlay">
-                        <a href="#">
-                            <img src="/frontend/assets/images/demos/demo-15/banners/banner-3.jpg" alt="Banner">
-                        </a>
-
-                        <div class="banner-content men">
-                            <h2 class="banner-title text-white">03. Beautiful <br>dresses perfect <br>for any occasion.</h2>
-                            <h3 class="banner-subtitle text-brightblack">IN THIS LOOK</h3><
-
-                            <ul class="text-white">
-                                <li>Botanical-Print Dress</li>
-                                <li>Cece Shoulder Bag</li>
-                                <li>Cunningham Leather Sandal</li>
-                            </ul>
-
-                            <div class="banner-text text-brightblack">130.00₺ - 358.00₺</div><!-- End .banner-text -->
-                            <a href="#" class="btn btn-outline-primary-2"><span>Kategori İncele</span><i class="icon-long-arrow-right"></i></a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-6">
-                    <div class="heading ">
-                        <h2 class="title text-center">Başlık Gelecek</h2>
-                        <p class="title-desc">Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır</p><!-- End .title-desc -->
-                        <p class="title-desc">Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır</p><!-- End .title-desc -->
-                    </div>
-
-                    <div class="row">
-                        <div class="col-6 col-md-4">
-                            <div class="product product-11 text-center">
-
-                                <figure class="product-media">
-                                    <a href="{{ route('urunler')}}">
-                                        <img src="/frontend/assets/images/demos/demo-2/products/product-1-1.jpg" alt="Product image" class="product-image">
-                                    </a>
-
-                                    <div class="product-action-vertical">
-                                        <a href="#" class="btn-product-icon btn-wishlist"><span>Favorilere Ekle</span></a>
-                                    </div>
-                                </figure>
-
-                                <div class="product-body">
-                                    <h3 class="product-title"><a href="{{ route('urunler')}}">Kitap Adı Gelecek</a></h3>
-                                    <div class="product-price">
-                                        251,00₺
-                                    </div>
-                                </div>
-                                <div class="product-action">
-                                    <a href="#" class="btn-product btn-cart"><span>Sepete Ekle</span></a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-6 col-md-4">
-                            <div class="product product-11 text-center">
-
-                                <figure class="product-media">
-                                    <a href="{{ route('urunler')}}">
-                                        <img src="/frontend/assets/images/demos/demo-2/products/product-1-1.jpg" alt="Product image" class="product-image">
-                                    </a>
-
-                                    <div class="product-action-vertical">
-                                        <a href="#" class="btn-product-icon btn-wishlist"><span>Favorilere Ekle</span></a>
-                                    </div>
-                                </figure>
-
-                                <div class="product-body">
-                                    <h3 class="product-title"><a href="{{ route('urunler')}}">Kitap Adı Gelecek</a></h3>
-                                    <div class="product-price">
-                                        251,00₺
-                                    </div>
-                                </div>
-                                <div class="product-action">
-                                    <a href="#" class="btn-product btn-cart"><span>Sepete Ekle</span></a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-6 col-md-4">
-                            <div class="product product-11 text-center">
-
-                                <figure class="product-media">
-                                    <a href="{{ route('urunler')}}">
-                                        <img src="/frontend/assets/images/demos/demo-2/products/product-1-1.jpg" alt="Product image" class="product-image">
-                                    </a>
-
-                                    <div class="product-action-vertical">
-                                        <a href="#" class="btn-product-icon btn-wishlist"><span>Favorilere Ekle</span></a>
-                                    </div>
-                                </figure>
-
-                                <div class="product-body">
-                                    <h3 class="product-title"><a href="{{ route('urunler')}}">Kitap Adı Gelecek</a></h3>
-                                    <div class="product-price">
-                                        251,00₺
-                                    </div>
-                                </div>
-                                <div class="product-action">
-                                    <a href="#" class="btn-product btn-cart"><span>Sepete Ekle</span></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
---}}
 
     <div class="deal-container" >
         <div class="container" >
@@ -514,8 +239,7 @@
         </div>
     </div>
 
-    <div class="mb-6"></div>
-    <div class="container" id="kitaplar">
+    <div class="container mt-3" id="kitaplar">
         <div class="row">
             @foreach($Product as $item)
                 <div class="col-6 col-md-3">
@@ -530,11 +254,6 @@
         </div>
 
     </div>
-    <div class="container">
-        <hr class="mt-1 mb-6">
-    </div>
-
-
 
     <div class="blog-posts">
         <div class="container">
@@ -600,37 +319,6 @@
 
         </div>
     </div>
-    {{--
-    <div class="container newsletter-popup-container mfp-hide" id="newsletter-popup-form">
-          <div class="row justify-content-center">
-              <div class="col-10">
-                  <div class="row no-gutters bg-white newsletter-popup-content">
-                      <div class="col-xl-3-5col col-lg-7 banner-content-wrap">
-                          <div class="banner-content text-center">
-                              <img src="/frontend/assets/images/tblogo.jpg" class="logo" alt="logo" width="60" height="15">
-                              <h2 class="banner-title">get <span>25<light>%</light></span> off</h2>
-                              <p>Kısa Açıklama Yazısı Gelecek</p>
-                              <form action="#">
-                                  <div class="input-group input-group-round">
-                                      <input type="email" class="form-control form-control-white" placeholder="Email Adresinizi Yazınız" aria-label="Email Adress" required>
-                                      <div class="input-group-append">
-                                          <button class="btn" type="submit"><span>gönder</span></button>
-                                      </div><!-- .End .input-group-append -->
-                                  </div><!-- .End .input-group -->
-                              </form>
-                              <div class="custom-control custom-checkbox">
-                                  <input type="checkbox" class="custom-control-input" id="register-policy-2" required>
-                                  <label class="custom-control-label" for="register-policy-2">Bir daha gösterme</label>
-                              </div><!-- End .custom-checkbox -->
-                          </div>
-                      </div>
-                      <div class="col-xl-2-5col col-lg-5 ">
-                          <img src="/frontend/assets/images/popup/newsletter/img-1.jpg" class="newsletter-img" alt="newsletter">
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>--}}
     @if(Cart::instance('lastLook')->content()->count()) > 0)
     <div class="container">
         <div class="row">
